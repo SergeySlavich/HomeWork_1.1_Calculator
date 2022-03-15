d7ca65b4b97ddcd56ff04a9820770b1105dfc23d
Makarov Sergey <kopcap_iv@mail.ru>
2022-03-16 00:03:34 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2); //1+2=3
+        int b = calc.minus.apply(1, 1); //1-1=0
+        int c = calc.devide.apply(a, b); // 3:0 - на 0 делить нельзя!
+
+        calc.println.accept(c);
+    }
+}
