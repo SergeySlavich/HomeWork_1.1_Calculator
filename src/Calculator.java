2650428b6a6938b59a223a58e2a9e6094dc51c6d
Makarov Sergey <kopcap_iv@mail.ru>
2022-03-17 19:03:08 +0500

    Devide by zero

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> (y == 0) ? Integer.valueOf("на 0 делить нельзя") : x / y;
+    BinaryOperator<Integer> devide = (x, y) -> (y != 0) ? x / y : (x > 0) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
 
-        int a = calc.plus.apply(1, 2); //1+2=3
-        int b = calc.minus.apply(1, 1); //1-1=0
-        int c = calc.devide.apply(a, b); // 3:0 - на 0 делить нельзя!
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
 
         calc.println.accept(c);
     }
